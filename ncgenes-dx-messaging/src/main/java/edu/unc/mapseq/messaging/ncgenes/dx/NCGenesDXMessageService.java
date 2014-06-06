de36b40c5c874521d9425a5bc6e9282e30f41a65
Jason Reilly <jdr0887@renci.org>
2014-06-06 16:06:21 -0400

    initial commit

---
+++ ncgenes-dx-messaging/src/main/java/edu/unc/mapseq/messaging/ncgenes/dx/NCGenesDXMessageService.java
@@ -0,0 +1,76 @@
+package edu.unc.mapseq.messaging.ncgenes.dx;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NCGenesDXMessageService {
+
+    private final Logger logger = LoggerFactory.getLogger(NCGenesDXMessageService.class);
+
+    private Connection connection;
+
+    private Session session;
+
+    private ConnectionFactory connectionFactory;
+
+    private NCGenesDXMessageListener messageListener;
+
+    private String destinationName;
+
+    public NCGenesDXMessageService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        this.connection = connectionFactory.createConnection();
+        this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = this.session.createQueue(this.destinationName);
+        MessageConsumer consumer = this.session.createConsumer(destination);
+        consumer.setMessageListener(getMessageListener());
+        this.connection.start();
+
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        if (this.session != null) {
+            this.session.close();
+        }
+        if (this.connection != null) {
+            this.connection.stop();
+            this.connection.close();
+        }
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public NCGenesDXMessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public void setMessageListener(NCGenesDXMessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
